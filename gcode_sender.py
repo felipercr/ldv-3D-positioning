7bb4413e02bd5606ab04b6b71f647fcecd939708
felipercr <felipercr@ufmg.br>
2022-08-16 15:40:48 -0300

    updates in the movement class

--- gcode_sender.py
+++ gcode_sender.py
@@ -2,13 +2,15 @@ import serial
 import time
 
 class movement():
-    def __init__(self, g):
+    def __init__(self):
 
         #Feedrate
         self.fdr = 1200
 
         #Serial communication
-        self.g = g
+        print('\nConnecting...')
+        self.g = serial_begin()
+        print('GRBL connected\n')
 
     def move(self, point):
 
@@ -17,6 +19,8 @@ class movement():
         z = float("{:.3f}".format(point[2]))
 
         self.g.flushInput()
+        self.g.flushOutput()
+        time.sleep(1)
 
         print(f'Command: X{x} Y{y} Z{z}')
 
@@ -28,7 +32,7 @@ class movement():
 
         inp = self.g.readline().decode().strip()
         if inp == 'ok': print('ok')
-        else: print('Error' + '\n\n')
+        else: print(f'Error: {inp}' + '\n\n' )
 
     def check_pos(self, point):
         self.g.flushOutput()
@@ -37,8 +41,19 @@ class movement():
         #time.sleep(1)
 
         pos = self.g.readline().decode()
-        pos = pos[11:27]
+
+        #If a hard limit stop occurs, GRBL will send
+        #a message -> [MSG:Reset to continue]
+        if pos == '[MSG:Reset to continue]' or pos == 'ALARM 1':
+            print('\nHard limits\n')
+            exit()
+
+        pos = pos.split('|')
+        pos = pos[1]
+        pos = pos.split(':')
+        pos = pos[1]
         pos = pos.split(',')
+
         for item in range(len(pos)): pos[item] = float(pos[item])
 
         x = float("{:.3f}".format(point[0]))
@@ -49,17 +64,30 @@ class movement():
         else: return False
 
     def home(self):
+        print('Homing cycle enabled...')
         self.g.write('$H\n'.encode())
         time.sleep(1)
 
-        inp = self.g.readline().decode().strip()
+        self.g.flushOutput()
+
+        while self.g.readline().decode().strip() != 'ok': pass 
+        print('Homing cycle completed\n')
+
+        #Close and reopen communication to reset MPos
+        self.g.close()
+        time.sleep(1)
+        self.g = serial_begin()
+
         pass
 
+    #Close serial communication
+    def finish(self):
+        self.g.close()
+
 def serial_begin():
-    print('\nConnecting...')
 
     try:
-        g = serial.Serial('COM5', 115200)
+        g = serial.Serial('COM3', 115200)
 
     except:
         print('Connection error\n')
@@ -73,5 +101,5 @@ def serial_begin():
     time.sleep(1)
 
     g.flushInput()
-    print('GRBL connected\n')
+
     return g
--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from gcode_sender import *
 from points_matrix import *
 
 def main():
-    g = serial_begin()
-
-    m = movement(g)
+    m = movement()
 
     matrix = points_matrix()
+
+    m.home()
 
     for line in matrix:
         m.move(line)
         while m.check_pos(line) == False: pass
+
+        #################LASER#################
+        #################LASER#################
+
         print('Chegou\n')
         time.sleep(1)
 
-    g.close()
+    m.finish()
 
 if __name__ == "__main__":
     main()
